aa09d49a95f1e6dd67a9abf362a22066ea28a1ac
Nicolas R. Torba <torbanr@NRT-MAC.local>
2018-06-28 13:44:25 -0400

    development testing works

--- answer_phone.py
+++ answer_phone.py
@@ -1,20 +1,47 @@
-from flask import Flask, url_for
+from flask import (
+   Flask,
+   Response,
+   redirect,
+   render_template,
+   request,
+   url_for,
+)
 from twilio.twiml.voice_response import VoiceResponse
-from on_call import get_oncall
+from on_call import get_oncall, get_schedules
+from twilio.twiml.voice_response import VoiceResponse, Gather
 
+
+schedules = get_schedules()
 app = Flask(__name__)
-
 @app.route("/answer", methods = ['GET','POST'])
+def get_team():
+    command = ''
+    for i in range(len(schedules)):
+        command += 'Press %d for %s.   ' %(i+1,schedules[i])
+   # command = "Press 1 for S.R.E.. Press 2 for Nick"
+    response = VoiceResponse()
+    gather = Gather(
+            num_digits=1,
+            action='/forward',
+            method='POST'
+            )
+    gather.say(command)
+    response.append(gather)
+    return (str(response), 200)
+
+@app.route('/forward', methods=['GET','POST'])
 def hello():
     """ REspond to incoming phone calls with a brief message. """
+    team = int(request.values.get('Digits', None))
     resp = VoiceResponse()
-
-    # Read a message aloud to the caller
-    resp.say("Thank you for calling! Have a great day.", voice = 'bob')
     
-    resp.dial(get_oncall())
+    # Read a message aloud to the caller
+    resp.say("You pressed the number for  %s. We will forward you now." % (schedules[team-1]), voice = 'bob') 
+    resp.dial(get_oncall(schedules[team-1]))
     return str(resp)
-
+'''
+Nick! Put the below code inside your /answer function call.
+'''
 if __name__ == "__main__":
     app.run(debug=True)
 
--- on_call.py
+++ on_call.py
@@ -1,17 +1,29 @@
 import requests
 import json
 
-def get_oncall():
-	api_key = 'axyyL8GMBsyQycLXJGkw'
-	url = 'https://api.pagerduty.com/oncalls?time_zone=UTC'
-	headers = {"Accept": "application/vnd.pagerduty+json;version=2","Authorization": "Token token=" + api_key}
-	r = requests.get(url, headers=headers)
-	data = json.loads(r.text)
-	on_call_users = []
-	for dat in data['oncalls']:
-		on_call_users.append(dat['user']['summary'])
-	with open('employee_numbers.json', 'r') as f:
-		data = json.load(f)
-	# Only returns on call users for one group
-	# If more than one group than need a loop to return multiple numbers
-	return data[on_call_users[0]]
+def get_oncall(schedule):
+    api_key = 'axyyL8GMBsyQycLXJGkw'
+    url = 'https://api.pagerduty.com/oncalls?time_zone=UTC'
+    headers = {"Accept": "application/vnd.pagerduty+json;version=2","Authorization": "Token token=" + api_key}
+    r = requests.get(url, headers=headers)
+    data = json.loads(r.text)
+    on_call_user = ''
+    for dat in data['oncalls']:
+        if dat['schedule']['summary'] == schedule:
+            on_call_user = dat['user']['summary']
+    with open('employee_numbers.json', 'r') as f:
+        data = json.load(f)
+    # Only returns on call users for one group
+    # If more than one group than need a loop to return multiple numbers
+    return data[on_call_user]
+
+def get_schedules():
+    api_key = 'axyyL8GMBsyQycLXJGkw'
+    url = 'https://api.pagerduty.com/schedules'
+    headers = {"Accept": "application/vnd.pagerduty+json;version=2","Authorization": "Token token=" + api_key}
+    r = requests.get(url, headers=headers)
+    data = json.loads(r.text)
+    schedules = []
+    for dat in data['schedules']:
+        schedules.append(dat['summary'])
+    return schedules
